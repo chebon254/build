310c4feb2da9d5e95135a1d98dabfc01cb4ef7c2
chebon254 <kelvinchebon90@gmail.com>
2022-05-27 17:02:59 +0300

    Few Changes

--- js/script.js
+++ js/script.js
@@ -70,8 +70,6 @@ function reveal() {
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
     }
   }
 }
