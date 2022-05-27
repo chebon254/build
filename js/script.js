f43ea2bbcaf8e363393a303ad78d9f4b76db3cd9
chebon254 <kelvinchebon90@gmail.com>
2022-05-27 15:57:31 +0300

    Animated Changes

--- js/script.js
+++ js/script.js
@@ -12,3 +12,69 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+/* == Sticky NavBar == */
+//Create A Responsive Menu Hide Show Side Menu
+const header = document.querySelector(".header");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const menu = document.querySelector(".links");
+const toTop = document.querySelector(".to-top");
+// Onscroll Change Nav Bar Background
+window.onscroll = ()=>{
+    this.scrollY > 20 ? header.classList.add("sticky") : header.classList.remove("sticky");
+}
+window.onscroll = ()=>{
+  this.scrollY > 40 ? toTop.classList.add("show") : toTop.classList.remove("show");
+}
+
+/* == Sticky NavBar == */
+
+/* == TESTIMONIAL TABS == */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("testimonial");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+
+}
+
+/* == TESTIMONIAL TABS == */
+
+
+
+/* == ANIMATION CSS == */
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 0;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+/* == ANIMATION CSS == */
